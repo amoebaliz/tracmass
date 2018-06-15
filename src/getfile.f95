36f7fcb1a4c8c5829bd8381b113605dc9776377c
amoebaliz <liz.drenkard@gmail.com>
2018-06-15 14:48:27 -0700

    returning allocation of x,y space to previous

--- src/getfile.f95
+++ src/getfile.f95
@@ -81,7 +81,8 @@ MODULE mod_getfile
     s = start2d(map2d)
     c = count2d(map2d)
     d = c + s - 1
-    allocate ( field(c(1),c(2)), get2dfieldNC(imt+2,jmt) )
+    allocate ( field(c(1),c(2)), get2dfieldNC(imt+2,jmt) ) ! LD: old issue with backtrace error
+    allocate ( field(c(1),c(2)), get2dfieldNC(imt,jmt) )   ! LD: leaves out last eta_rho xi_rho
     field=0; get2dfieldNC=0
 
     ierr=NF90_OPEN(trim(fieldFile) ,NF90_NOWRITE ,ncid)
